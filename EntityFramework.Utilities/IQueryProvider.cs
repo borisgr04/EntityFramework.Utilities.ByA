a91f00eb77483118a1b01b526f417395715e992f
Boris Arturo <borisgr04@hotmail.com>
2018-02-21 20:59:31 -0500

    Agregar archivos de proyecto.

---
+++ EntityFramework.Utilities/IQueryProvider.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace EntityFramework.Utilities
+{
+    public interface IQueryProvider
+    {
+        bool CanDelete { get; }
+        bool CanUpdate { get; }
+        bool CanInsert { get; }
+        bool CanBulkUpdate { get; }
+        SqlTransaction Transaction { get; set; }
+
+        string GetDeleteQuery(QueryInformation queryInformation);
+        string GetUpdateQuery(QueryInformation predicateQueryInfo, QueryInformation modificationQueryInfo);
+        void InsertItems<T>(IEnumerable<T> items, string schema, string tableName, IList<ColumnMapping> properties, DbConnection storeConnection, int? batchSize);
+        void UpdateItems<T>(IEnumerable<T> items, string schema, string tableName, IList<ColumnMapping> properties, DbConnection storeConnection, int? batchSize, UpdateSpecification<T> updateSpecification);
+
+        bool CanHandle(DbConnection storeConnection);
+
+
+        QueryInformation GetQueryInformation<T>(System.Data.Entity.Core.Objects.ObjectQuery<T> query);
+
+
+    }
+}
